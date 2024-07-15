a3e01019308c9e9b220a2b7018a88592cfc29ce8
Harry Dhillon <hdhillon478@gmail.com>
2024-07-15 16:01:31 +0100

    Build out the header component

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
+import Logo from "./Logo";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/events/all", label: "All Events" },
+];
 
 export default function Header() {
   return (
-    <header>
-      <Link href='/'>Home</Link>
-      <Link href='/events/all'>Events</Link>
+    <header className='flex justify-between items-center border-b border-white/20 h-14 sm:px-9 px-3'>
+      <Logo />
+      <nav>
+        <ul className='flex gap-x-6 text-sm'>
+          {links.map(({ href, label }) => (
+            <li
+              key={href}
+              className='text-white/70 hover:text-white transition'
+            >
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
