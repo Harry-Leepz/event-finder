911dd569b1681934b6c283d2976ab675da7e2f55
Harry Dhillon <hdhillon478@gmail.com>
2024-07-15 16:25:55 +0100

    Add tailwind styling to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,30 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Discover Amazing Events Nearby!</h1>
-      <p>
-        Explore a world of excitement with hundreds of local events just around
-        the corner.
+    <main className='flex items-center flex-col pt-36 px-3'>
+      <h1 className='text-3xl lg:text-6xl font-bold tracking-tight'>
+        Discover Amazing Events Nearby
+      </h1>
+      <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-90'>
+        Explore a world of{" "}
+        <span className='font-bold italic underline text-pink-500'>
+          excitement
+        </span>{" "}
+        with events just around the corner.
       </p>
 
-      <form>
+      <form className='w-full sm:w-[580px]'>
         <input
+          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-pink-400 transition focus:ring-2 focus:bg-white/10'
           type='text'
           spellCheck={false}
           placeholder='Search for events in any city...'
         />
       </form>
 
-      <section>
+      <section className='mt-4 flex gap-x-4 text-sm text-white/50'>
         <p>Popular Locations:</p>
-        <div>
+        <div className='space-x-2 font-semibold'>
           <Link href='/events/london'>London</Link>
           <Link href='/events/manchester'>Manchester</Link>
           <Link href='/events/birmingham'>Birmingham</Link>
