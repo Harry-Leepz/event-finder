004feef623711302c045b34d4a994ef28271cab1
Harry Dhillon <hdhillon478@gmail.com>
2024-07-15 16:02:10 +0100

    Wip: Add text content to app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main>
+      <h1>Discover Amazing Events Nearby!</h1>
+      <p>
+        Explore a world of excitement with hundreds of local events just around
+        the corner.
+      </p>
+
+      <form>
+        <input
+          type='text'
+          spellCheck={false}
+          placeholder='Search for events in any city...'
+        />
+      </form>
+
+      <section>
+        <p>Popular Locations:</p>
+        <div>
+          <Link href='/events/london'>London</Link>
+          <Link href='/events/manchester'>Manchester</Link>
+          <Link href='/events/birmingham'>Birmingham</Link>
+        </div>
+      </section>
+    </main>
+  );
 }
