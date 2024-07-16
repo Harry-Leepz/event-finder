1707c55608acf536eb7dc38ab071db2d566ef2ed
Harry Dhillon <hdhillon478@gmail.com>
2024-07-16 16:25:04 +0100

    Small style updates to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
       </h1>
       <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-90'>
         Explore a world of{" "}
-        <span className='font-bold italic underline text-pink-500'>
+        <span className='font-bold italic underline text-accent'>
           excitement
         </span>{" "}
-        with events just around the corner.
+        with events just around the corner
       </p>
 
       <form className='w-full sm:w-[580px]'>
         <input
-          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-pink-400 transition focus:ring-2 focus:bg-white/10'
+          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent transition focus:ring-2 focus:bg-white/10'
           type='text'
           spellCheck={false}
           placeholder='Search for events in any city...'
@@ -24,11 +24,17 @@ export default function Home() {
       </form>
 
       <section className='mt-4 flex gap-x-4 text-sm text-white/50'>
-        <p>Popular Locations:</p>
-        <div className='space-x-2 font-semibold'>
-          <Link href='/events/london'>London</Link>
-          <Link href='/events/manchester'>Manchester</Link>
-          <Link href='/events/birmingham'>Birmingham</Link>
+        <p>Popular:</p>
+        <div className='space-x-2 font-semibold '>
+          <Link className='hover:text-accent' href='/events/london'>
+            London
+          </Link>
+          <Link className='hover:text-accent' href='/events/manchester'>
+            Manchester
+          </Link>
+          <Link className='hover:text-accent' href='/events/birmingham'>
+            Birmingham
+          </Link>
         </div>
       </section>
     </main>
