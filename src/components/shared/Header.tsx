7a04e2fa69fd63c829100df37d12026abcc5c77d
Harry Dhillon <hdhillon478@gmail.com>
2024-07-16 16:42:43 +0100

    Add logic to show active link styling

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const links = [
   { href: "/", label: "Home" },
@@ -7,6 +11,8 @@ const links = [
 ];
 
 export default function Header() {
+  const activePathname = usePathname();
+
   return (
     <header className='flex justify-between items-center border-b border-white/20 h-14 sm:px-9 px-3'>
       <Logo />
@@ -15,7 +21,10 @@ export default function Header() {
           {links.map(({ href, label }) => (
             <li
               key={href}
-              className='text-white/70 hover:text-white transition'
+              className={clsx(" hover:text-white transition", {
+                "text-white": activePathname === href,
+                "text-white/50": activePathname !== href,
+              })}
             >
               <Link href={href}>{label}</Link>
             </li>
