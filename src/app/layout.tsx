5056693794609125e12fde671344a6d556ee3648
Harry Dhillon <hdhillon478@gmail.com>
2024-07-16 16:24:08 +0100

    Add container as wrapper around app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
+import Container from "@/components/shared/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} bg-gray-950 text-gray-50`}>
-        <Header />
-        {children}
-        <Footer />
+        <Container>
+          <Header />
+          {children}
+          <Footer />
+        </Container>
       </body>
     </html>
   );
