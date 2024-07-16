bc936841d4eb82b80d49da180f56b0a347bba8fb
Harry Dhillon <hdhillon478@gmail.com>
2024-07-16 16:24:36 +0100

    Build out and add styling to footer component

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
+const links = [
+  { href: "/privacy-policy", label: "Privacy Policy" },
+  { href: "/terms-conditions", label: "Terms and Conditions" },
+];
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='mt-auto flex justify-between items-center border-t border-white/20 h-16 px-3 sm:px-9 text-xs text-white/25'>
+      <small className='text-xs'>
+        All content is fictional and is solely for education purposes.
+      </small>
+
+      <ul className='flex gap-x-3 sm:gap-x-8'>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
