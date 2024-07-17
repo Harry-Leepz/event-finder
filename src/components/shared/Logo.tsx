6bf2010e424f82718d1fdebb89a2cb08c3f1733f
Harry Dhillon <hdhillon478@gmail.com>
2024-07-17 11:26:19 +0100

    Wrap logo component in a link back to home page

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "../../../public/logo.svg";
 
 export default function Logo() {
-  return <Image src={logo} alt='Eventy logo' width={53} height={12} />;
+  return (
+    <Link href='/'>
+      <Image src={logo} alt='Eventy logo' width={53} height={12} />
+    </Link>
+  );
 }
