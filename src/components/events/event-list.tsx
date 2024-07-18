bca986cff323440b18a96ba2221cdcceb8541587
Harry Dhillon <hdhillon478@gmail.com>
2024-07-18 10:17:34 +0100

    Update event list styling

--- src/components/events/event-list.tsx
+++ src/components/events/event-list.tsx
@@ -7,7 +7,7 @@ type EventListProps = {
 
 export default function EventList({ events }: EventListProps) {
   return (
-    <section>
+    <section className='flex flex-wrap gap-10 justify-center max-w-[1100px] px-[20px]'>
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
