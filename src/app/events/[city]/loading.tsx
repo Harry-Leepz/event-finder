982bc17b8e2dee1f13de23925e978d2e3d276a35
Harry Dhillon <hdhillon478@gmail.com>
2024-07-20 17:58:17 +0100

    Fix: reduce padding top

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -2,7 +2,7 @@ import SkeletonCard from "@/components/skeletons/skeleton-card";
 
 export default function Loading() {
   return (
-    <div className='flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-40 gap-20'>
+    <div className='flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-24 gap-20'>
       {Array.from({ length: 9 }).map((_, i) => (
         <SkeletonCard key={i} />
       ))}
