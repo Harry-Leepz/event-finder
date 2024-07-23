7f13e74d467383dc65b2a38a46d20461269b0bfd
Harry Dhillon <hdhillon478@gmail.com>
2024-07-23 14:26:49 +0100

    Remove button from the not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,14 +13,7 @@ export default function NotFound() {
 
   return (
     <main className='flex items-center justify-center pt-24 flex-col '>
-      <MainHeading>Wooops this page does not exist!!!</MainHeading>
-
-      <button
-        onClick={handleClick}
-        className='bg-white/20 border-2 border-white/10 text-lg capitalize rounded-md py-2 w-[75%] sm:w-[50%]  bg-blur state-effect mt-40'
-      >
-        Take me back home
-      </button>
+      <MainHeading> 404 Wooops this page does not exist!!!</MainHeading>
     </main>
   );
 }
