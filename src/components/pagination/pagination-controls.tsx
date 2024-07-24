c0eec9090aa8e2e2a6cb5b63ca77e1b3b6ea2e9f
Harry Dhillon <hdhillon478@gmail.com>
2024-07-24 11:31:32 +0100

    Add pagination-controls component with styling

---
+++ src/components/pagination/pagination-controls.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+const paginationBtnStyles =
+  "text-white px-5 flex items-center gap-x-2 py-2 rounded-md hover:bg-white/10 opacity-80 transition text-sm";
+
+type PaginationControlsProps = {
+  previousPath: string;
+  nextPath: string;
+};
+
+export default function PaginationControls({
+  previousPath,
+  nextPath,
+}: PaginationControlsProps) {
+  return (
+    <section className='flex w-full justify-between'>
+      <Link className={paginationBtnStyles} href={previousPath}>
+        <ArrowLeftIcon />
+        Previous
+      </Link>
+      <Link className={paginationBtnStyles} href={nextPath}>
+        Next
+        <ArrowRightIcon />
+      </Link>
+    </section>
+  );
+}
