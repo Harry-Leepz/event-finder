3be33223c1bf6cda447f4d0d8f415fbe022b83d7
Harry Dhillon <hdhillon478@gmail.com>
2024-07-25 14:57:39 +0100

    chore: Update getEvents function to use Next.js unstable_cache

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import prisma from "./db";
 import { notFound } from "next/navigation";
+import { unstable_cache } from "next/cache";
 
 // Wrapper around clsx that merges the classes with tailwind-merge
 export function cn(...inputs: ClassValue[]) {
@@ -20,7 +21,7 @@ export function capitalizeFirstLetter(str: string) {
 }
 
 // Fetch events from the API based on the city
-export async function getEvents(city: string, page = 1) {
+export const getEvents = unstable_cache(async (city: string, page = 1) => {
   const events = await prisma.eventyEvent.findMany({
     where: {
       city: city === "all" ? undefined : capitalizeFirstLetter(city),
@@ -39,10 +40,10 @@ export async function getEvents(city: string, page = 1) {
   });
 
   return { events, totalCountOfEvents };
-}
+});
 
 // Fetch a single event from the API based on the slug
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventyEvent.findUnique({
     where: {
       slug: slug,
@@ -53,4 +54,4 @@ export async function getEvent(slug: string) {
     return notFound();
   }
   return event;
-}
+});
